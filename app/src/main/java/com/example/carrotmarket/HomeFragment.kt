d8189c11db686c217a987f48d1e23a2dd331122d
jiyoung2387 <ghkd1310@naver.com>
2023-09-18 23:22:22 +0900

    2 week mission completed

--- app/src/main/java/com/example/carrotmarket/HomeFragment.kt
+++ app/src/main/java/com/example/carrotmarket/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.carrotmarket
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +16,24 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        //기능 추가
+        //상품 아이템 박스 선택하면 상세 상품 페이지로 넘어가도록 설정
+        binding.clItem1.setOnClickListener {
+            val intent = Intent(requireContext(), StuffInfoActivity::class.java)
+            startActivity(intent)
+        }
+
+        //알람 아이콘 선택하면 알람 페이지로 넘어가도록 설정
+        binding.ivHomeAlarm.setOnClickListener {
+            val intent = Intent(requireContext(), AlarmActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
         return binding.root
+
     }
 
 }
