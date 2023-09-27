9bc89df84bdc8d105b5dd7522455d02a06da19fb
MinseoSONG <mins041600@naver.com>
2023-09-28 01:35:41 +0900

    3주차 미션 완료

--- app/src/main/java/com/example/carrotmarket/MyCarrotpage.kt
+++ app/src/main/java/com/example/carrotmarket/MyCarrotpage.kt
@@ -5,17 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.carrotmarket.databinding.FragmentMyBinding
+import com.example.carrotmarket.databinding.FragmentMyCarrotpageBinding
 
 class MyCarrotFragment : Fragment() {
-    lateinit var binding : FragmentMyBinding
+    lateinit var binding : FragmentMyCarrotpageBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMyBinding.inflate(layoutInflater)
+        binding = FragmentMyCarrotpageBinding.inflate(layoutInflater)
         return binding.root
     }
 }
