b0e378dcf1374e94ae4a5c6e7bddb3d5998c7167
TaejungKim <tae0803@naver.com>
2023-10-01 13:48:20 +0900

    3주차 커밋
    1. my 페이지 레이아웃 구현
    2. 메인 화면 글쓰기 버튼 구현
    3. 메인 상세 화면 하단 bar 고정

--- app/src/main/java/com/example/carrotmarket/AlarmActivity.kt
+++ app/src/main/java/com/example/carrotmarket/AlarmActivity.kt
@@ -1,13 +1,9 @@
 package com.example.carrotmarket
 
-import android.opengl.Visibility
-import android.os.Build.VERSION_CODES.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isGone
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
+
 import com.example.carrotmarket.databinding.ActivityAlarmBinding
 
 class AlarmActivity : AppCompatActivity() {
