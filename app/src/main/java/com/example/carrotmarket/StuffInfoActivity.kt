8180b6d2701ec893924b097b68ac267d9a7d002c
MinseoSONG <mins041600@naver.com>
2023-11-03 21:54:43 +0900

    6주차 미션 완료

--- app/src/main/java/com/example/carrotmarket/StuffInfoActivity.kt
+++ app/src/main/java/com/example/carrotmarket/StuffInfoActivity.kt
@@ -4,12 +4,18 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.viewpager2.widget.ViewPager2
 import com.example.carrotmarket.databinding.ActivityStuffInfoBinding
 
 class StuffInfoActivity : AppCompatActivity() {
     lateinit var binding: ActivityStuffInfoBinding
     var imgList = mutableListOf<String>()
+
+    private val pagerHandler = Handler(Looper.getMainLooper())
+    private val imageSwiper = ImageSwiper()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityStuffInfoBinding.inflate(layoutInflater)
@@ -31,6 +37,36 @@ class StuffInfoActivity : AppCompatActivity() {
         initDummyData()
         initViewPager()
 
+        imageSwiper.start()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        imageSwiper.interrupt()
+    }
+
+    inner class ImageSwiper: Thread(){
+        override fun run() {
+            try{
+                while(true){
+                    sleep(1000)
+                    pagerHandler.post{
+                        var position = binding.imageAreaVp.currentItem
+                        if(position==2){
+                            position=0
+                        }else{
+                            position++
+                        }
+
+                        binding.imageAreaVp.currentItem=position
+                    }
+                }
+            }catch (e: InterruptedException){
+                Log.d("INTERRUPT","쓰레드 종료")
+                interrupt()
+            }
+        }
     }
 
     private fun initViewPager() {
@@ -41,14 +77,15 @@ class StuffInfoActivity : AppCompatActivity() {
     private fun initDummyData() {
         imgList.add("https://cdn.pixabay.com/photo/2018/05/01/18/21/eclair-3366430_1280.jpg")
         imgList.add("https://cdn.pixabay.com/photo/2017/05/04/21/23/cupcakes-2285209_1280.jpg")
+        imgList.add("https://cdn.pixabay.com/photo/2018/07/01/19/50/muffin-3510308_1280.jpg")
     }
 
-    private fun initView(data: String?) {
-        if (data == null) {
-            finish()
-            return
-        }
-
-        // TODO: 받은 데이터를 StuffInfoActivity의 해당하는 View에 적용해보기
-}
+//    private fun initView(data: String?) {
+//        if (data == null) {
+//            finish()
+//            return
+//        }
+//
+//        // TODO: 받은 데이터를 StuffInfoActivity의 해당하는 View에 적용해보기
+//}
 }
