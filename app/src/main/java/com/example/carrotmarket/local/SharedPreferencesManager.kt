f391ad9e17beb17b749797bd2595b355aa75c32f
star8041 <mhn07001@naver.com>
2024-02-12 17:20:03 +0900

    Jwt 토큰 중간 저장

--- app/src/main/java/com/example/carrotmarket/local/SharedPreferencesManager.kt
+++ app/src/main/java/com/example/carrotmarket/local/SharedPreferencesManager.kt
@@ -3,13 +3,13 @@ package com.example.carrotmarket.local
 import com.example.carrotmarket.ApplicationClass
 
 fun getJwt() : String? {
-    return ApplicationClass.mSharedPreferences.getString()
+    return ApplicationClass.mSharedPreferences.getString("abcd",null)
 }
 
 fun removeJwt() {
     ApplicationClass.mSharedPreferences.edit().clear().apply()
 }
 
-fun saveJwt() {
-    ApplicationClass.mSharedPreferences.edit().putString()
+fun saveJwt(token : String) {
+    ApplicationClass.mSharedPreferences.edit().putString("abcd",token).apply()
 }
